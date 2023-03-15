a81c3bcb913b730f206eb8b4b368d53f23dbb13f
VNaas <viktor.enaas@gmail.com>
2023-03-15 11:39:27 +0100

    test led with ROS

--- board_interface/scripts/blinker.py
+++ board_interface/scripts/blinker.py
@@ -1,25 +1,31 @@
 #!/usr/bin/env python
 import rospy
-from std_msgs.msg import String
-import board
+from std_msgs.msg import Float32
+import board as brd
+import colorsys
+import board as brd
 
-def callback(data):
-    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
-
+def callback(data, board):
+    # rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+    color = colorsys.hsv_to_rgb(data,1,1)
+    test = [int(color[0] * 255), int(color[1] * 255), int(color[2] * 255)]
+    board.send(test)
     
-def listener():
+def blinker(board):
 
     # In ROS, nodes are uniquely named. If two nodes with the same
     # name are launched, the previous one is kicked off. The
     # anonymous=True flag means that rospy will choose a unique
-    # name for our 'listener' node so that multiple listeners can
+    # name for our 'blinker' node so that multiple blinkers can
     # run simultaneously.
-    rospy.init_node('listener', anonymous=True)
+    rospy.init_node('blinker', anonymous=True)
 
-    rospy.Subscriber("chatter", String, callback)
+    rospy.Subscriber("pinger", Float32, callback,board)
 
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
 
 if __name__ == '__main__':
-    listener()
+    board = brd.Board()
+    board.init('/dev/ttyACM0')
+    blinker(board)
--- board_interface/scripts/pinger.py
+++ board_interface/scripts/pinger.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python
 # license removed for brevity
 import rospy
-from std_msgs.msg import String
+from std_msgs.msg import Float32
 
 def pinger():
-    pub = rospy.Publisher('pinger', String, queue_size=10)
+    pub = rospy.Publisher('pinger', Float32, queue_size=10)
     rospy.init_node('pinger', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
+    rate = rospy.Rate(250) # 0.004ms
+    hue = 0
+
     while not rospy.is_shutdown():
-        ping_ = "ping %s" % rospy.get_time()
-        rospy.loginfo(ping_)
-        pub.publish(ping_)
+        hue += 0.004
+        if hue > 1.0:
+            hue -= 1.0
+        # ping_ = "ping %s" % rospy.get_time()
+        # rospy.loginfo(ping_)
+        pub.publish(hue)
         rate.sleep()
 
 if __name__ == '__main__':
